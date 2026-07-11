c1fceb5a2a002a04f4c96f71322e114720086bd2
Soofa <youssefjoo2255@gmail.com>
2026-07-11 21:02:34 +0300

    Understanding middlewares

--- 03-ExpressJS/app.js
+++ 03-ExpressJS/app.js
@@ -5,6 +5,18 @@ const express = require('express')
 
 const app = express();
 
+// app.use() registers a middleware function in Express.
+app.use((req, res, next) => {
+    console.log("in the middleware");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("In another middleware!"); 
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+
 // Callback function => executed when the request happens
 const server = http.createServer(app);
 
---
+++ 03-ExpressJS/moduleSummary.md
@@ -0,0 +1,5 @@
+## MiddleWare:
+
+- Middleware means some of autmation to the incoming request by some of functions instead of just one request handler does everything (Core Concept of Express js). 
+
+- It's a middleware between the request and response
